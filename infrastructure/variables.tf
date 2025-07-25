e70006e80a82332354b677cba17f573c68c0d791
11bthornton <11bthornton@gmail.com>
2025-07-25 19:44:52 +0100

    add action

---
+++ infrastructure/variables.tf
@@ -0,0 +1,21 @@
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  type        = string
+  default     = "rg-tf-deployment-testing"
+}
+
+variable "location" {
+  description = "The Azure region where the resource group will be created"
+  type        = string
+  default     = "East US"
+}
+
+variable "tags" {
+  description = "A map of tags to assign to the resource group"
+  type        = map(string)
+  default = {
+    Environment = "Development"
+    Project     = "tf-deployment-testing"
+    ManagedBy   = "Terraform"
+  }
+}
