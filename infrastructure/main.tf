b175d33410ec8737f79fec28e932acfdc3b22ee6
11bthornton <11bthornton@gmail.com>
2025-07-25 21:09:49 +0100

    test

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -7,7 +7,7 @@ resource "azurerm_resource_group" "main" {
 
 
 resource "azurerm_resource_group" "sdfsdf" {
-  name     = "sdfasdasasdasddas"
+  name     = "sdfasdasasdasdda ss ss"
   location = var.location
 
   tags = var.tags
