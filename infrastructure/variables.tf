fc59381da9c0d62cde55f6dd2421b3eab5dd6422
Ben Thornton <11bthornton@gmail.com>
2025-07-25 20:09:26 +0100

    Update variables.tf

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -5,7 +5,7 @@ variable "resource_group_name" {
 }
 
 variable "location" {
-  description = "The Azure region where the resource group will be created"
+  description = "The Azure region  sdf where the resource group will be created"
   type        = string
   default     = "East US"
 }
